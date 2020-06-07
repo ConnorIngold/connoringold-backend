cce145369afd7194ec9b10ee7d0a8a7171cac482
connoringold <connoringold@gmail.com>
2020-06-07 16:02:12 +0100

    added rate limter

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const limiter = new RateLimiter(5, "hour")
 const whitelist = ['https://connoringold.com/', 'https://connoringold.com/contact.html', "https://connoringoldcontactform.herokuapp.com/contact", "http://localhost:3000", "https://connoringoldcontactform.herokuapp.com/contact"]
 const cors = require("cors")
 
-// Middleware
-// app.enable('trust proxy');
-// app.use(function (req, res, next) {
-//   if (req.secure) {
-//     return next();
-//   }
-//   res.redirect("https://" + req.headers.host + req.url);
-// });
+Middleware
+app.enable('trust proxy');
+app.use(function (req, res, next) {
+  if (req.secure) {
+    return next();
+  }
+  res.redirect("https://" + req.headers.host + req.url);
+});
 app.use(express.static('public'))
 app.use(morgan('dev'))
 app.use(express.json())
