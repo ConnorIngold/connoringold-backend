08918a10241316afa0755be2f23727f0f90216b1
connoringold <connoringold@gmail.com>
2021-01-07 14:33:46 +0000

    text update and contact form update

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 
 const app = express()
-const morgan = require('morgan');
-const nodemailer = require('nodemailer');
+const morgan = require('morgan')
+const nodemailer = require('nodemailer')
 const port = process.env.PORT || 3000
-const path = require('path');
+const path = require('path')
 
 // Antispam protection
-const RateLimiter = require("limiter").RateLimiter
-const limiter = new RateLimiter(5, "hour")
+const RateLimiter = require('limiter').RateLimiter
+const limiter = new RateLimiter(5, 'hour')
 
-const whitelist = ['https://connoringold.com/', 'https://connoringold.com/contact.html', "https://connoringoldcontactform.herokuapp.com/contact", "http://localhost:3000", "https://connoringoldcontactform.herokuapp.com/contact"]
-const cors = require("cors")
+const whitelist = [
+	'https://connoringold.com/',
+	'https://connoringold.com/contact.html',
+	'http://connoringoldcontactform.herokuapp.com/contact',
+	'http://localhost:3000',
+	'https://connoringoldcontactform.herokuapp.com/contact',
+]
+const cors = require('cors')
 
 // // Middleware
 // app.use(function (req, res, next) {
@@ -26,57 +32,60 @@ app.use(express.static('public'))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(
-  cors({
-    origin: whitelist,
-  })
+	cors({
+		origin: whitelist,
+	})
 )
 
 app.post('/contact', (req, res) => {
-  // Step one login
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "login",
-      user: "connoringold@gmail.com",
-      pass: "oyrphgbmagibyopt",
-    },
-  })
+	// Step one login
+	let transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			type: 'login',
+			user: 'connoringold@gmail.com',
+			pass: 'oyrphgbmagibyopt',
+		},
+	})
 
-  // Step two email content options
-  let mailOptions = {
-    from: req.body.email,
-    to: "connoringold@gmail.com",
-    subject: `Email from ${req.body.name}`,
-    text: req.body.message
-  }
-  limiter.removeTokens(1, (err, remainingRequests) => {
-    console.log("remainingRequests: ", remainingRequests);
-    
-    if (remainingRequests < 1) {
-      res.writeHead(429, { "Content-Type": "text/plain;charset=UTF-8" })
-      res.end("429 Too Many Requests - your IP is being rate limited")
-    } else {   
-      // Step three send email
-      transporter.sendMail(mailOptions, (err, data) => {
-        if (err) {
-          console.log("Error Occurs: ", err)
-          res.json({"error ": err})
-        } else {
-          console.log("Email sent!!!")
-          res.json({ "email data: ": data })
-        }
-      })
-    }
-  })
+	// Step two email content options
+	let mailOptions = {
+		from: req.body.email,
+		to: 'connoringold@gmail.com',
+		subject: `Email from ${req.body.name}`,
+		text: req.body.message,
+	}
+	limiter.removeTokens(1, (err, remainingRequests) => {
+		console.log('remainingRequests: ', remainingRequests)
 
-  
+		if (remainingRequests < 1) {
+			res.writeHead(429, { 'Content-Type': 'text/plain;charset=UTF-8' })
+			res.end('429 Too Many Requests - your IP is being rate limited')
+		} else {
+			// Step three send email
+			transporter.sendMail(mailOptions, (err, data) => {
+				if (err) {
+					console.log('Error Occurs: ', err)
+					res.json({ 'error ': err })
+				} else {
+					console.log('Email sent!!!')
+					res.json({ 'email data: ': data })
+				}
+			})
+		}
+	})
 })
 
+app.get('/', (req, res) =>
+	res.sendFile(path.join(__dirname + '/public/projects.html'))
+)
+app.get('/projects', (req, res) =>
+	res.sendFile(path.join(__dirname + '/public/projects.html'))
+)
+app.get('/contact', (req, res) =>
+	res.sendFile(path.join(__dirname + '/public/contact.html'))
+)
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/projects.html')))
-app.get('/projects', (req, res) => res.sendFile(path.join(__dirname +'/public/projects.html')))
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/public/contact.html')))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-
+app.listen(port, () =>
+	console.log(`Example app listening at http://localhost:${port}`)
+)
